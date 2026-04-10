7ef410b4aab7c03b42046ef92b9ed59af117967d
Андрей Повикалов <apovikalov@bk.ru>
2026-04-10 18:06:17 +0600

    Контроллеры

--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -1,3 +1,12 @@
+# catalog/views.py
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+def about(request):
+    return render(request, 'catalog/about.html')
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')

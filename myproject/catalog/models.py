500ccb431f29e6d00d1146beae0b137192a608c7
Андрей Повикалов <apovikalov@bk.ru>
2026-04-20 10:38:25 +0600

    23. Работа с ORM в Django

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -1,24 +1,6 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
-    category = models.CharField(max_length=30, verbose_name='Категория')
-    price = models.FloatField(verbose_name='Цена за покупку')
-    created_at = models.DateField(verbose_name='Дата создания')
-    last_changed_at = models.DateField(verbose_name='Дата последнего изменения')
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name']
-
-
 class Category(models.Model):
     name = models.CharField(max_length=30, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
@@ -29,4 +11,22 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.FloatField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания')
+    last_changed_at = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
         ordering = ['name']

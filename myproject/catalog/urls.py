5f91d2a4a18448edf7788e558c23fc30910f28df
Андрей Повикалов <apovikalov@bk.ru>
2026-04-26 15:10:57 +0600

    Базовый шаблон

--- myproject/catalog/urls.py
+++ myproject/catalog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('base/', views.base, name='base'),
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
     path('products/', views.products, name='products'),
--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -7,6 +7,10 @@ from catalog.models import Category, Product
 # from django.http import HttpResponse
 
 
+def base(request):
+    return render(request, 'catalog/base.html')
+
+
 def home(request):
     product = Product.objects.get(name='Продукт 1')
     context = {'product': product, 'all_products': Product.objects.all()}

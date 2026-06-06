a1733d7ad3695ad3fe28150aeef4190380b5865d
Андрей Повикалов <apovikalov@bk.ru>
2026-06-06 17:04:18 +0600

    Создание группы, назначение прав

--- myproject/catalog/groups.py
+++ myproject/catalog/groups.py
@@ -4,3 +4,5 @@ moderator_group = Group.objects.create(name='Модератор продукто
 
 delete_permission = Permission.objects.get(codename='delete_modelname')
 unpublish_permission = Permission.objects.get(codename='can_unpublish_product')
+
+moderator_group.permissions.add(delete_permission, unpublish_permission)
